17ee18ceb2a188426d634fb9fa3c866d7dad4809
Gabriel Guilherme Carvalho Viana <guilherme.gabriel@academico.ifrn.edu.br>
2020-02-21 17:44:09 -0300

    Exercicio 1 da Lista 2

---
+++ Lista 2/Exercício 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva dois valores inteiros");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b) Console.WriteLine($"Maior = {a}");
+            else Console.WriteLine($"Maior = {b}");
+            Console.ReadKey();
+        }
+    }
+}
