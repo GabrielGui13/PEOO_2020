ae86c5cc586a1983b1d67dd15f3deeccbe2784c0
Gabriel Guilherme <gabrielguilherme13@hotmail.com>
2020-02-23 21:06:57 -0300

    atualização

---
+++ Lista 2/Exercicio 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os coeficientes a, b e c de uma equação do II grau");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            double bp = (-b + Math.Sqrt(Math.Pow(b, 2) - 4*a*c)) / (2*a);
+            double bn = (-b - Math.Sqrt(Math.Pow(b, 2) - 4*a*c)) / (2*a);
+
+            if ((bp % 1 == 0) && (bn % 1 == 0)) Console.WriteLine($"As raizes são {bp} e {bn}");
+            else Console.WriteLine("Impossível Calcular");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lista 2/Exercicio 8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 4 valores inteiros");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+
+            int soma = n1 + n2 + n3 + n4;
+            int maior = n1;
+            int menor = n1;
+
+            if (n2 > maior) maior = n2;
+            if (n3 > maior) maior = n3;
+            if (n4 > maior) maior = n4;
+
+            if (n2 < menor) menor = n2;
+            if (n3 < menor) menor = n3;
+            if (n4 < menor) menor = n4;
+
+            int final = soma - (maior + menor);
+
+            Console.WriteLine($"Maior valor = {maior}");
+            Console.WriteLine($"Menor valor = {menor}");
+            Console.WriteLine($"A soma do segundo maior valor com o segundo menor = {final}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
