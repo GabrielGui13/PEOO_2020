b39c0ee9af09876237e7938d6ed1060bca7d66c1
Gabriel Guilherme <gabrielguilherme13@hotmail.com>
2020-03-06 17:55:49 -0300

    att

---
+++ Torneio GITHUB/Q. H/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q.H
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] entry = Console.ReadLine().Split(' ');
+            int X = int.Parse(entry[0]);
+            int O = int.Parse(entry[1]);
+            int C = int.Parse(entry[2]);
+            int soma = X + O + C;
+
+            
+
+        }
+    }
+}
