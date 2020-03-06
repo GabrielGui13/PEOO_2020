eddd1418fdb464d1170af4ef87c74abd596f4b86
Gabriel Guilherme <gabrielguilherme13@hotmail.com>
2020-03-06 17:15:30 -0300

    att

---
+++ Torneio GITHUB/Q. F/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q.F
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= N; x++)
+            {
+                if (N % x == 0) Console.WriteLine(x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
